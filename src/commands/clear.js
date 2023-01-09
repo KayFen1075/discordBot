e85281c7ec4e80a0a1887db11d1a37a84351b829
KayFen <on100161@gmail.com>
2023-01-09 08:09:36 +0200

    Убрал некоторый хлам и начал делать неко-торые команды

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,6 +1,8 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+
 const clearMessage = new SlashCommandBuilder()
     .setName('clear')
     .setDescription('Очистить сообщения')
     .addNumberOption((option) => option.setName('count').setDescription('Количество сообщений').setRequired(true))
+
 export default clearMessage.toJSON();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { config } from "dotenv";
 
-import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { ActionRow, ChannelManager, Client, discordSort, GatewayIntentBits, Message, Routes, StringSelectMenuBuilder, StringSelectMenuComponent } from 'discord.js';
 import { REST } from "@discordjs/rest";
 import clearMessages from './commands/clear.js'
+import moneyMessage from './commands/money.js'
+import list from './commands/list.js'
+import { ActionRowBuilder, EmbedBuilder, SelectMenuBuilder, SelectMenuOptionBuilder } from "@discordjs/builders";
 
 config();
 const TOKEN = process.env.BOT_TOKEN;
@@ -19,7 +22,7 @@ const rest = new REST({ version: '10'}).setToken(TOKEN);
 
 // Запуск слеш команд
 async function main() {
-    const commands = [clearMessages]
+    const commands = [clearMessages, moneyMessage, list]
     try {
         console.log('Начало проверки команд (/)');
         await rest.put(Routes.applicationCommands('1060761793204596846'), { body: commands });
@@ -30,35 +33,59 @@ async function main() {
     }
 }; main();
 
+
 client.on('ready', ()=>{
     console.log(`${client.user.tag} активирован!`);
 })
 
 client.on('interactionCreate', (interaction) => {
-    if (interaction.isChatInputCommand() && interaction.commandName == 'remove-money') {
-        interaction.reply({ content: `Был создан тикет по запросу на снятие с карты ${interaction.options.get('card').value}`})
+    if (interaction.isChatInputCommand() && interaction.commandName === 'list') {
+        console.log('Use `list` command');
+       if (interaction.options.getSubcommand() === 'remove') {
+        console.log('Use subcommand `list remove`');
+
+        const menu = new ActionRowBuilder().setComponents(
+            new SelectMenuBuilder()
+            .setCustomId('games-selectiopn').setOptions([
+                {label: '1', value: '1'},
+                {label: '2', value: '2'}
+            ])
+            )
+
+        interaction.reply({
+            components: [menu.toJSON()]
+        })
+       }
     }
-    if (interaction.memberPermissions) {
-        if (interaction.isChatInputCommand() && interaction.commandName == 'clear') {
-            const count = interaction.options.get('count').value
-            interaction.channel.bulkDelete(count)
-            interaction.reply(`Удалено ${count} сообщений!`)
-            console.log(`Удалено ${count} сообщений!`);
-            setTimeout(()=>interaction.deleteReply(), 5000)
-        }
+
+    
+    if (interaction.isChatInputCommand() && interaction.commandName == 'clear') { // Команда Clear
+        const count = interaction.options.get('count').value
+        interaction.channel.bulkDelete(count)
+        interaction.reply(`Удалено ${count} сообщений!`)
+        console.log(`Удалено ${count} сообщений! из канала ${interaction.channel.name}(${interaction.channelId})`);
+        setTimeout(()=>interaction.deleteReply(), 5000)
     }
 })
+
+
 
 client.on('messageCreate', (message)=>{
     if (message.author.bot == false) {
         if (message.content.toString().toLocaleUpperCase().search('КОГДА') >= 0) {
-            const replyMessages = ['Завтра', 'Сейчас', 'Вчера', 'Послезавтра', `Через ${Math.floor(Math.random(1, 30))}д`];
+            const replyMessages = ['Завтра', 'Сейчас', 'Вчера', 'Послезавтра', `Через ${Math.floor(Math.random(0) * 31)}д`];
             message.reply(replyMessages[Math.floor(Math.random(0) * replyMessages.length)])
         }
         console.log( `${message.author.username}: ${message.content} ///#${message.channel.name}\\${message.channelId} ~ ${message.createdAt.toDateString()}`);
-    }
-})
+    console.log(bot);
+    const logChannel = bot.channels.cache.filter('1060771841452560507');
+    logChannel.send(`Пользователь ${message.author.tag}(${message.author.id}) отправил сообщение:
+                    ${message.content}`)
+}})
 
-client.on('guildMemberAdd', (member) => {
-    console.log('На сервер зашёл человек');
+
+client.on('ready', (a) => {
+    client.user.setActivity(`👉👌💦`);
+    client.user.setStatus('idle');
+    
 })
